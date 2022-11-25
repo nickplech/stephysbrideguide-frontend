dea77637951d2db32b73c9465a95023e472f5f99
Nick Plechas <nickplechas@gmail.com>
2022-11-25 11:11:16 -0800

    checkit

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,11 +2,7 @@ export default {
   openGraph: {
     type: 'website',
     locale: 'en_IE',
+    url: 'https://www.stephysbrideguide.com',
     site_name: 'StephysBrideGuide',
-  },
-  // twitter: {
-  //   handle: '@handle',
-  //   site: '@site',
-  //   cardType: 'summary_large_image',
-  // },
+  }
 }
